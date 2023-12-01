b0947b2d2f807a0e86caa4ea2e044a558d6cf77d
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-12-01 10:06:54 +0400

    6.1

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -6,18 +6,35 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
+  console.log(state)
   switch (action.type) {
     case 'GOOD':
+      state = {
+        ...state,
+        good: state.good + 1
+      }
       return state
     case 'OK':
+      state = {
+        ...state,
+        ok: state.ok + 1
+      }
       return state
     case 'BAD':
+      state = {
+        ...state,
+        bad: state.bad + 1
+      }
       return state
     case 'ZERO':
+      state = {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
       return state
     default: return state
   }
-  
 }
 
 export default counterReducer
